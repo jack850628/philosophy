4fec360920e125bf74be6fcb3122847fbd05eb52
Andrew424 <a878966424a@gmail.com>
2019-11-10 01:14:29 +0800

    彩虹材質與結算畫面

---
+++ Assets/Scripts/End.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class End : MonoBehaviour
+{
+    [SerializeField] GameObject[] img;
+    [SerializeField] Vector3[] pos;
+    [SerializeField] float duration; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator end()
+    {
+        for (int k = 0; k < img.Length; k++)
+        {
+            yield return new WaitForSeconds(duration);
+            for (int i = 0; i < img.Length; i++)
+            {
+
+                img[i].transform.DOMove(pos[i], duration);
+            }
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/Settlement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Settlement : MonoBehaviour
+{
+    //物件動態
+    [SerializeField] Transform[] MoveObjs;
+    [SerializeField] Vector3[] target_pos;
+    [SerializeField] float[] duration;
+    [SerializeField] Ease _moveEase = Ease.Linear;
+    //背景
+    [SerializeField] Transform[] BackGround;
+    [SerializeField]Vector3 mini, max;
+    [SerializeField]float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Start_Settlement());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < BackGround.Length; i++)
+        {
+            BackGround[i].position = new Vector3(BackGround[i].position.x-speed, BackGround[i].position.y, BackGround[i].position.z);
+            if (BackGround[i].position.x < mini.x)
+            {
+                BackGround[i].position = max;
+            }
+            
+        }
+        
+    }
+    IEnumerator Start_Settlement()
+    {
+        yield return new WaitForSeconds(1);
+        for (int i = 0; i < target_pos.Length; i++)
+        {
+            MoveObjs[i].DOMove(target_pos[i], duration[i]);
+        }
+    }
+}
